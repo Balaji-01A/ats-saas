4e82338ee781249faa2e464e3cd6663ea522c0d4
Balaji-01A <balajireddy5000@gmail.com>
2026-04-07 18:46:14 +0530

    Fix app not defined error

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,10 @@
 from flask import Flask
 
-# create Flask app FIRST
 app = Flask(__name__)
 
-# route
 @app.route("/")
 def home():
     return "ATS SaaS Backend Running 🚀"
 
-# run app
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
